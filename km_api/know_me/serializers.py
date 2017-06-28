c99bdc7812c034ddfeaec5552264178615ede905
Chathan Driehuys <cdriehuys@gmail.com>
2017-06-27 23:04:07 -0400

    Add profile serializer.

---
+++ km_api/know_me/serializers.py
@@ -0,0 +1,16 @@
+"""Serializers for the models in the ``know_me`` module.
+"""
+
+from rest_framework import serializers
+
+from know_me import models
+
+
+class ProfileDetailSerializer(serializers.ModelSerializer):
+    """
+    Serializer for ``Profile`` instances.
+    """
+
+    class Meta:
+        fields = ('id', 'name', 'quote', 'welcome_message')
+        model = models.Profile
---
+++ km_api/know_me/tests/serializers/test_profile_detail_serializer.py
@@ -0,0 +1,18 @@
+from know_me import serializers
+
+
+def test_serialize(profile_factory):
+    """
+    Test serializing a profile.
+    """
+    profile = profile_factory()
+    serializer = serializers.ProfileDetailSerializer(profile)
+
+    expected = {
+        'id': profile.id,
+        'name': profile.name,
+        'quote': profile.quote,
+        'welcome_message': profile.welcome_message,
+    }
+
+    assert serializer.data == expected
