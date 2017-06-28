84acc00a3f6d09b4212b6728667af583b45e5a99
Chathan Driehuys <cdriehuys@gmail.com>
2017-06-28 15:06:29 -0400

    Add test for creating profile from serializer.

--- km_api/know_me/tests/serializers/test_profile_list_serializer.py
+++ km_api/know_me/tests/serializers/test_profile_list_serializer.py
@@ -1,6 +1,30 @@
 from know_me import serializers
 
 
+def test_create(user_factory):
+    """
+    Saving a serializer containing valid data should create a new
+    profile.
+    """
+    user = user_factory()
+    data = {
+        'name': 'John',
+        'quote': "Hi, I'm John",
+        'welcome_message': 'This is my profile.',
+    }
+
+    serializer = serializers.ProfileListSerializer(data=data)
+    assert serializer.is_valid()
+
+    serializer.save(user=user)
+    profile = user.profile
+
+    assert profile.name == data['name']
+    assert profile.quote == data['quote']
+    assert profile.welcome_message == data['welcome_message']
+    assert profile.user == user
+
+
 def test_serialize(profile_factory):
     """
     Test serializing a profile.
