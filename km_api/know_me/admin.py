81d121c83997e6fd3a756f507aa7a2406451b3ee
Chathan Driehuys <cdriehuys@gmail.com>
2017-06-30 01:40:09 -0400

    Register profile item model in admin.

--- km_api/know_me/admin.py
+++ km_api/know_me/admin.py
@@ -2,6 +2,7 @@
 """
 
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from know_me import models
 
@@ -35,6 +36,46 @@ class ProfileGroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'profile', 'is_default')
     list_filter = ('is_default',)
     search_fields = ('name', 'profile__name')
+
+
+@admin.register(models.ProfileItem)
+class ProfileItemAdmin(admin.ModelAdmin):
+    """
+    Admin for the ``ProfileItem`` model.
+    """
+    fields = ('name', 'row', 'gallery_item', 'text')
+    list_display = ('name', 'get_profile', 'get_group', 'row')
+    search_fields = ('name',)
+
+    def get_group(self, item):
+        """
+        Get the profile group the profile item belongs to.
+
+        Args:
+            item:
+                The profile item to get the parent group of.
+
+        Returns:
+            The parent profile group of the given profile item.
+        """
+        return item.row.group
+    get_group.admin_order_field = 'row__group'
+    get_group.short_description = _('group')
+
+    def get_profile(self, item):
+        """
+        Get the profile the profile item belongs to.
+
+        Args:
+            item:
+                The profile item to get the parent profile of.
+
+        Returns:
+            The parent profile of the given profile item.
+        """
+        return item.row.group.profile
+    get_profile.admin_order_field = 'row__group__profile'
+    get_profile.short_description = _('profile')
 
 
 @admin.register(models.ProfileRow)
