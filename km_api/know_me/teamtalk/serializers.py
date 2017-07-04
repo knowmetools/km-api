8c6a498bbcac0713dd046264eb38e6587319698e
Chathan Driehuys <cdriehuys@gmail.com>
2017-07-04 15:30:02 -0400

    Add serializer for generating a Layer identity.

---
+++ km_api/know_me/teamtalk/serializers.py
@@ -0,0 +1,26 @@
+"""Serializers for the ``know_me.teamtalk`` module.
+"""
+
+from rest_framework import serializers
+
+from know_me.teamtalk import layer
+
+
+class IdentitySerializer(serializers.Serializer):
+    """
+    Serializer for obtaining an identity from Layer.
+
+    This serializer takes a nonce provided by Layer and obtains an
+    identity for the user making the request. This identity can then be
+    used to participate in conversations.
+    """
+    identity_token = serializers.CharField(read_only=True)
+    nonce = serializers.CharField(write_only=True)
+
+    def save(self):
+        """
+        Generate an identity for the requesting user.
+        """
+        self.identity_token = layer.generate_identity_token(
+            self.context['request'].user,
+            self.nonce)
