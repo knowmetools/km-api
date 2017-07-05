c5a1f12becbe76f383ee637be0f4f280c679124e
Chathan Driehuys <cdriehuys@gmail.com>
2017-07-05 01:15:26 -0400

    Remove unused profile view mixin.

--- km_api/know_me/mixins.py
+++ km_api/know_me/mixins.py
@@ -6,24 +6,6 @@ from django.shortcuts import get_object_or_404
 from know_me import models
 
 
-class ProfileMixin:
-    """
-    Mixin for retrieving profiles.
-
-    This mixin restricts the retrieved profiles to those accessible by
-    the user making the request.
-    """
-
-    def get_queryset(self):
-        """
-        Get the profiles that the requesting user has access to.
-
-        Returns:
-            The profiles owned by the requesting user.
-        """
-        return models.Profile.objects.filter(user=self.request.user)
-
-
 class ProfileGroupMixin:
     """
     Mixin for retrieving profile groups.
