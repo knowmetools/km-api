fea78d70d563a53a99c4ea9866d6136da30bc38d
Chathan Driehuys <cdriehuys@gmail.com>
2017-07-06 00:10:14 -0400

    Fix URL typo.

--- km_api/know_me/urls.py
+++ km_api/know_me/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^profiles/(?P<pk>[0-9])/$', views.ProfileDetailView.as_view(), name='profile-detail'),                    # noqa
     url(r'^profiles/(?P<pk>[0-9])/gallery/$', views.GalleryView.as_view(), name='gallery'),                         # noqa
     url(r'^profiles/(?P<pk>[0-9])/groups/$', views.ProfileGroupListView.as_view(), name='profile-group-list'),      # noqa
-    url(r'^profiles/(?P<pk>[0-9])/teamtalk/$', include('know_me.teamtalk.urls', namespace='teamtalk')),             # noqa
+    url(r'^profiles/(?P<pk>[0-9])/teamtalk/', include('know_me.teamtalk.urls', namespace='teamtalk')),              # noqa
     url(r'^rows/(?P<pk>[0-9]+)/$', views.ProfileRowDetailView.as_view(), name='profile-row-detail'),                # noqa
     url(r'^rows/(?P<pk>[0-9]+)/items/$' ,views.ProfileItemListView.as_view(), name='profile-item-list'),            # noqa
 ]
