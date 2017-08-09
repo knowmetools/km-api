c3fff88ec538a5edcd47304409c9ac711b124ce1
audsc <audsc@users.noreply.github.com>
2017-08-09 12:12:35 -0400

    Add KM User Accessor Model (#141)
    Closes #129

---
+++ km_api/know_me/tests/models/test_km_user_accessor_model.py
@@ -0,0 +1,24 @@
+from know_me import models
+
+
+def test_create(km_user_factory, user_factory):
+    """
+    Test creating a KMUserAccessor.
+    """
+    models.KMUserAccessor.objects.create(
+        accepted=False,
+        can_read_everywhere=False,
+        can_write_everywhere=False,
+        km_user=km_user_factory(),
+        user_with_access=user_factory())
+
+
+def test_string_conversion(km_user_accessor_factory):
+    """
+    Converting a km user accessor to a string should return the kmuser accessor
+    factory's name.
+    """
+    accessor = km_user_accessor_factory()
+
+    expected = 'Accessor for {user}'.format(user=accessor.km_user.name)
+    assert str(accessor) == expected
