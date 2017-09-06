f875a64539f186cd2858dff8fe401a54780ca048
Chathan Driehuys <cdriehuys@gmail.com>
2017-09-06 01:20:42 -0400

    Update serializer to use 'share' method.
    The KMUserAccessorSerializer now uses the KMUser.share method to create
    a new accessor.

--- km_api/know_me/serializers/km_user_accessor_serializers.py
+++ km_api/know_me/serializers/km_user_accessor_serializers.py
@@ -9,18 +9,33 @@ from .km_user_serializers import KMUserDetailSerializer
 
 class KMUserAccessorSerializer(serializers.ModelSerializer):
     """
-    Serializer for multiple ``KMUserAccessor`` instances.
+    Serializer for ``KMUserAccessor`` instances.
     """
     km_user = KMUserDetailSerializer(read_only=True)
-    user_with_access_email = serializers.EmailField(
-            source='user_with_access.email',
-            read_only=True)
 
     class Meta:
         fields = (
             'accepted',
             'can_write',
+            'email',
             'has_private_profile_access',
             'km_user',
-            'user_with_access_email',)
+        )
         model = models.KMUserAccessor
+        read_only_fields = ('accepted',)
+
+    def create(self, validated_data):
+        """
+        Create a new accessor for a Know Me user.
+
+        Args:
+            validated_data (dict):
+                The data to create the accessor from.
+
+        Returns:
+            The newly created ``KMUserAccessor`` instance.
+        """
+        km_user = validated_data.pop('km_user')
+        email = validated_data.pop('email')
+
+        return km_user.share(email, **validated_data)
--- km_api/know_me/tests/serializers/test_km_user_accessor_serializer.py
+++ km_api/know_me/tests/serializers/test_km_user_accessor_serializer.py
@@ -1,4 +1,32 @@
+from unittest import mock
+
 from know_me import serializers
+
+
+def test_create(km_user_factory):
+    """
+    Saving the serializer should use the Know Me user's ``share`` method
+    to create a new accessor.
+    """
+    km_user = km_user_factory()
+    data = {
+        'can_write': True,
+        'email': 'test@example.com',
+        'has_private_profile_access': True,
+    }
+
+    serializer = serializers.KMUserAccessorSerializer(data=data)
+    assert serializer.is_valid()
+
+    with mock.patch.object(km_user, 'share', autospec=True) as mock_share:
+        serializer.save(km_user=km_user)
+
+    assert mock_share.call_count == 1
+    assert mock_share.call_args[0] == (data['email'],)
+    assert mock_share.call_args[1] == {
+        'can_write': data['can_write'],
+        'has_private_profile_access': data['has_private_profile_access'],
+    }
 
 
 def test_serialize(
@@ -13,6 +41,7 @@ def test_serialize(
     km_user = km_user_factory()
     user = user_factory()
     km_user_accessor = km_user_accessor_factory(
+        email=user.email,
         km_user=km_user,
         user_with_access=user)
 
@@ -27,9 +56,9 @@ def test_serialize(
     expected = {
         'accepted': False,
         'can_write': False,
+        'email': km_user_accessor.email,
         'has_private_profile_access': False,
         'km_user': km_user_serializer.data,
-        'user_with_access_email': user.email,
     }
 
     assert serializer.data == expected
