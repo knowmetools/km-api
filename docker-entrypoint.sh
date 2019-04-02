de99c594c05aa0aa19ad921ef11d349a2ec2c2b4
Chathan Driehuys <chathan@knowmetools.com>
2019-04-01 23:23:20 -0400

    Collect static files during migration.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,16 @@
 
 set -e
 
-if [ "$1" = 'migrate' ]; then
-    exec python /opt/km-api/km_api/manage.py migrate
+MANAGE_PATH=/opt/km-api/km_api/manage.py
+MANAGE_CMD="python $MANAGE_PATH"
+
+if [[ "$1" = 'migrate' ]]; then
+    ${MANAGE_CMD} migrate
+    ${MANAGE_CMD} collectstatic --no-input
+    exit 0
 fi
 
-if [ "$1" = 'server' ]; then
+if [[ "$1" = 'server' ]]; then
     # First shift to pop 'server' off the arg list. The rest of the arguments
     # are passed as-is to Gunicorn.
     shift
@@ -14,7 +19,7 @@ if [ "$1" = 'server' ]; then
     exec gunicorn km_api.wsgi:application $@
 fi
 
-if [ "$1" = '' ]; then
+if [[ "$1" = '' ]]; then
     echo "No command provided."
     exit 1
 fi
