01e1ce2fd8a75442bd5af9135986fb0c11d83f0f
Chathan Driehuys <chathan@knowmetools.com>
2019-04-11 14:53:38 -0400

    Add test for multiple Apple receipts.
    Added a regression test for #444 that tests uploading Apple receipts for
    multiple users.

--- km_api/functional_tests/know_me/subscriptions/test_set_apple_subscription.py
+++ km_api/functional_tests/know_me/subscriptions/test_set_apple_subscription.py
@@ -3,11 +3,11 @@ import datetime
 import pytest
 from django.utils import timezone
 from rest_framework import status
-from rest_framework.reverse import reverse
 
 from test_utils import serialized_time
 
 PREMIUM_PRODUCT_CODE = "premium"
+URL = "/know-me/subscription/apple/"
 
 
 @pytest.fixture(autouse=True)
@@ -37,8 +37,7 @@ def test_set_duplicate_apple_receipt(
     # ...and attempts to upload the same receipt data that is already in
     # use...
     data = {"receipt_data": receipt_data}
-    url = reverse("know-me:apple-subscription-detail")
-    response = api_client.put(url, data)
+    response = api_client.put(URL, data)
 
     # ...then he should receive a 400 response with an error indicating
     # the receipt is already in use.
@@ -68,11 +67,68 @@ def test_set_invalid_apple_receipt(
     # ...then when she attempts to set her receipt for her Know Me
     # subscription to an invalid receipt...
     data = {"receipt_data": receipt_data}
-    url = reverse("know-me:apple-subscription-detail")
-    response = api_client.put(url, json=data)
+    response = api_client.put(URL, json=data)
 
     # ...she should receive a 400 response.
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_set_multiple_apple_receipts(
+    api_client, apple_receipt_client, user_factory
+):
+    """
+    Multiple users should be able to add an Apple receipt to their
+    account.
+
+    Regression test for #444
+    """
+    expires = timezone.now().replace(microsecond=0) + datetime.timedelta(
+        days=30
+    )
+    receipt_1 = "receipt-1"
+    receipt_2 = "receipt-2"
+
+    # Given two users, Sarah and John...
+    password = "password"
+    user1 = user_factory(first_name="Sarah", password=password)
+    user2 = user_factory(first_name="John", password=password)
+
+    # ...who both have valid receipts for recognized products...
+    apple_receipt_client.enqueue_status(
+        receipt_1,
+        {
+            "status": 0,
+            "latest_receipt_info": [
+                {
+                    "expires_date_ms": str(int(expires.timestamp() * 1000)),
+                    "product_id": PREMIUM_PRODUCT_CODE,
+                }
+            ],
+        },
+    )
+    apple_receipt_client.enqueue_status(
+        receipt_2,
+        {
+            "status": 0,
+            "latest_receipt_info": [
+                {
+                    "expires_date_ms": str(int(expires.timestamp() * 1000)),
+                    "product_id": PREMIUM_PRODUCT_CODE,
+                }
+            ],
+        },
+    )
+
+    # ...then both of them should be able to set Apple receipts.
+    api_client.log_in(user1.primary_email.email, password)
+    response = api_client.put(URL, json={"receipt_data": receipt_1})
+
+    assert response.status_code == status.HTTP_200_OK
+
+    api_client.log_in(user2.primary_email.email, password)
+    response = api_client.put(URL, json={"receipt_data": receipt_2})
+
+    assert response.status_code == status.HTTP_200_OK
 
 
 def test_set_valid_apple_receipt(
@@ -108,8 +164,7 @@ def test_set_valid_apple_receipt(
 
     # ...then when he sets that receipt as his Know Me subscription...
     data = {"receipt_data": receipt_data}
-    url = reverse("know-me:apple-subscription-detail")
-    response = api_client.put(url, json=data)
+    response = api_client.put(URL, json=data)
     response_data = response.json()
 
     # ...he should receive a 200 response.
